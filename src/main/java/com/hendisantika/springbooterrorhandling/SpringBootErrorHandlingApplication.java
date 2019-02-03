6dc180beba4ebbe5e8838b0446d388a7b58b371e
Hendi Santika <hendisantika@gmail.com>
2019-02-03 08:50:31 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbooterrorhandling/SpringBootErrorHandlingApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbooterrorhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootErrorHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootErrorHandlingApplication.class, args);
+    }
+
+}
+
